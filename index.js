9db96413d5d2d04191c5d6024e5fc0a5d2ac9d06
Fedora19 <luc.viala19@gmail.com>
2016-03-21 17:30:11 +0100

    - Ajout des vues et des assets css et js

--- index.js
+++ index.js
@@ -10,39 +10,12 @@
 //var server = require('http').createServer(app);
 //var io = require('socket.io')(server);
 
-//var morgan = require('morgan');
+var morgan = require('morgan');
 
 var port = process.env.port || 1337;
 
 // Import & déclaration du logger
 var Logger = require('./assets/logger')('index');
-
-// Import de la librairie generator
-//var generator = require('./assets/generator.js')(__dirname, 'generatedFile.xml');
-
-// Import du fichier de configuration
-//var config_file = require('./config.json');
-
-// Import de la configurationd de l'application
-//var constants_file = require('./application.json');
-
-// Permet d'initialiser le générateur XML
-//generator.initialization();
-
-// Permet d'ajouter des templates dans la configuration
-//generator.addTemplate(config_file.config.templates[1]);
-
-//generator.addFolder("inputs");
-//generator.addFolder("ouputs");
-
-//generator.addInput("TAmb", "RTD", "inputs");
-
-// Permet de générer le fichier terminé
-//generator.generateSAXFile();
-
-//server.listen(port, function(){
-//  Logger.info('Server is listening on port '+port);
-//});
 
 
 var ApplicationClass = require(__dirname+'\\assets\\Application.js');
